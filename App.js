a7c265c78cdcac431385658df21ccb4199a69794
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2025-07-25 15:09:53 -0400

    using Button

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 import icon from './assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image blurRadius={10} source={icon} style={{ width: 200, height: 100, resizeMode: 'center'}}/>
-      <Image source={{uri: "https://cdn-images.dzcdn.net/images/cover/4333981d6071855ef2f893ea11a33b49/0x1900-000000-80-0-0.jpg"}} style={{ width: 200, height: 100}}/>
-      <Text style={{color:'white'}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="light" />
+      <Image source={icon} style={{ width: 200, height: 100, resizeMode: 'center'}}/>
+      <Image source={{uri: "https://cdn-images.dzcdn.net/images/cover/4333981d6071855ef2f893ea11a33b49/0x1900-000000-80-0-0.jpg"}} style={{ width: 200, height: 100}}/>
+      <Text style={{color:'white'}}>Tenemos aqui la pp!</Text>
+      {/*Este Button no es customizable salvo por el color y texto. No tiene styles*/}
+      <Button color='green' title="Pulsa aquí" onPress={() => alert('Hola')}/> 
     </View>
   );
 }
