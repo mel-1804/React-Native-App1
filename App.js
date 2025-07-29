e2b89b77fb22659a5ce5f46081e1759434676b3b
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2025-07-28 21:09:57 -0400

    ahora si Bien configurado prettier y ESlint

--- App.js
+++ App.js
@@ -1,34 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, TouchableHighlight, TouchableOpacity, Pressable } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableHighlight,
+  TouchableOpacity,
+  Pressable,
+} from 'react-native'
 
-import icon from './assets/icon.png';
+import icon from './assets/icon.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <Image source={icon} style={{ width: 200, height: 100, resizeMode: 'center'}}/>
-      <Image source={{uri: "https://cdn-images.dzcdn.net/images/cover/4333981d6071855ef2f893ea11a33b49/0x1900-000000-80-0-0.jpg"}} style={{ width: 200, height: 100}}/>
-      <Text style={{color:'white'}}>Tenemos aqui la pp!</Text>
+      <Image
+        source={icon}
+        style={{ width: 200, height: 100, resizeMode: 'center' }}
+      />
+      <Image
+        source={{
+          uri: 'https://cdn-images.dzcdn.net/images/cover/4333981d6071855ef2f893ea11a33b49/0x1900-000000-80-0-0.jpg',
+        }}
+        style={{ width: 200, height: 100 }}
+      />
+      <Text style={{ color: 'white' }}>Tenemos aqui la pp!</Text>
       {/*Este Button no es customizable salvo por el color y texto. No tiene styles--------------------------------------------------------*/}
-      <Button 
-        color='green' 
-      title="Pulsa aquí" 
-      onPress={() => alert('Hola')}/>
+      <Button color="green" title="Pulsa aquí" onPress={() => alert('Hola')} />
 
-       {/*TouchableHighlight---------------------------------------------------------------------------------------------------------------*/}
+      {/*TouchableHighlight---------------------------------------------------------------------------------------------------------------*/}
       <TouchableHighlight
-        underlayColor={"#09F"}
+        underlayColor={'#09F'}
         onPress={() => alert('Hola desde TouchableHighlight')}
-        style={{width: 200, height: 200, backgroundColor: 'red', borderRadius: 100, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{color: 'white'}}>Pulsa aquí</Text>  
-      </TouchableHighlight> 
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: 'red',
+          borderRadius: 100,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: 'white' }}>Pulsa aquí</Text>
+      </TouchableHighlight>
 
       {/*TouchableOpacity-------------------------------------------------------------------------------------------------------------------*/}
       <TouchableOpacity
         onPress={() => alert('Hola desde TouchableOpacity')}
-        style={{width: 200, height: 200, backgroundColor: 'blue', borderRadius: 100, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{color: 'white'}}>Pulsa aquí</Text>  
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: 'blue',
+          borderRadius: 100,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: 'white' }}>Pulsa aquí</Text>
       </TouchableOpacity>
 
       {/*Pressable---------------------------------------------------------------------------------------------------------------------------*/}
@@ -37,18 +67,18 @@ export default function App() {
         style={({ pressed }) => [
           {
             backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
-                        
           },
           styles.wrapperCustom,
-        ]}>
-          {({pressed}) => (
-            <Text style={
-              {fontSize: pressed? 32 : 16,}}>{pressed ? 'Pressed!' : 'Press Me'}</Text>
-          )}
-          </Pressable>
-        
+        ]}
+      >
+        {({ pressed }) => (
+          <Text style={{ fontSize: pressed ? 32 : 16 }}>
+            {pressed ? 'Pressed!' : 'Press Me'}
+          </Text>
+        )}
+      </Pressable>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -58,4 +88,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
-const prettierConfig = require('eslint-config-prettier');
+const { defineConfig } = require('eslint/config')
+const expoConfig = require('eslint-config-expo/flat')
+const prettierConfig = require('eslint-config-prettier')
 
 module.exports = defineConfig([
   expoConfig,
@@ -11,9 +11,9 @@ module.exports = defineConfig([
       prettier: require('eslint-plugin-prettier'),
     },
     rules: {
-      'prettier/prettier': 'warn',
+      'prettier/prettier': ['warn', require('./.prettierrc')],
     },
   },
   // Agrega prettier como extensión de configuración
   prettierConfig,
-]);
+])
