2c5a069d91bd2dc044bdddb09c28c0586d38a737
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2025-07-28 20:58:33 -0400

    configurado prettier y ESlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
-const prettier = require("eslint-config-prettier");
+const prettierConfig = require('eslint-config-prettier');
 
 module.exports = defineConfig([
   expoConfig,
@@ -11,8 +11,9 @@ module.exports = defineConfig([
       prettier: require('eslint-plugin-prettier'),
     },
     rules: {
-      // Prettier como regla de ESLint
       'prettier/prettier': 'warn',
     },
   },
+  // Agrega prettier como extensión de configuración
+  prettierConfig,
 ]);
